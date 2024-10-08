b1afc967df0827d2d71a8960bbacb618a0aafdf8
aman <amanraj1172004@gmail.com>
2024-10-08 15:28:14 +0530

    fornt setup

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,10 +1,21 @@
 import React from 'react'
 import { Routes,Route } from 'react-router-dom'
+import About from './pages/About'
+import Home from './pages/Home'
+import Projects from './pages/Projects'
+import Signin from './pages/Signin'
+import Signup from './pages/Signup'
+import Dashboard from './pages/Dashboard'
 const App = () => {
   return (
     <>
       <Routes>
-        <Route/>
+        <Route path='/about' element={<About/>} />
+        <Route path='/' element={<Home/>} />
+        <Route path='/project' element={<Projects/>} />
+        <Route path='/signin' element={<Signin/>} />
+        <Route path='/signup' element={<Signup/>} />
+        <Route path='/dashboard' element={<Dashboard/>} />
       </Routes>
     </>
   )
