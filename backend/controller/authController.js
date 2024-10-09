333886a3cc292c2e6617c5ae0e18d3f59a982cb0
aman <amanraj1172004@gmail.com>
2024-10-09 07:20:55 +0530

    sign

---
+++ backend/controller/authController.js
@@ -0,0 +1,31 @@
+import User from "../models/userModel.js";
+import bcryptjs from 'bcryptjs'
+import{errorHandler} from '../utils/error.js'
+
+export const signup = async(req,res,next)=>{
+const {username, email, password}= req.body;
+
+if(!username
+     || !email 
+     || !password
+      || username === ''
+       || email === '' 
+       || password === '')
+       {
+        next(errorHandler(400, 'All fields are required'));
+}
+
+const hashedPassword = bcryptjs.hashsync(password,10);
+ const newUser = new User({
+    username,
+    email,
+    password:hashedPassword,
+ })
+
+ try {
+    await newUser.save();
+res.json({message:'user created'})
+ } catch (error) {
+    next(error);
+}
+ }
--- backend/server.js
+++ backend/server.js
@@ -1,6 +1,8 @@
 import express from 'express'
 import mongoose from 'mongoose';
 import dotenv from 'dotenv'
+import userRoutes from './routes/userRoute.js'
+import authRoutes from './routes/authRoute.js'
 dotenv.config();
 mongoose
 .connect(
@@ -9,13 +11,33 @@ mongoose
 .then(()=>{
     console.log('db connected')
 });
-
 const app = express()
+app.use(express.json())
+const PORT = process.env.PORT || 3000
 
-app.get('/',(req,res)=>{
-    res.send("hello")
+
+
+
+app.use('/api/user',userRoutes)
+app.use('/api/auth',authRoutes)
+
+
+
+
+
+
+
+
+app.listen(PORT,()=>{
+    console.log(`server connected ${PORT}`)
 });
 
-app.listen(3000,()=>{
-    console.log("server connected 3000")
-});
+app.use((err, req, res, next) => {
+    const statusCode = err.statusCode || 500;
+    const message = err.message || 'Internal Server Error';
+    res.status(statusCode).json({
+      success: false,
+      statusCode,
+      message,
+    });
+  });
