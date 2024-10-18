9160664e9b12e015d8b58267381e282328f9864a
aman <amanraj1172004@gmail.com>
2024-10-18 22:48:09 +0530

    post

--- frontend/src/components/DashSidebar.jsx
+++ frontend/src/components/DashSidebar.jsx
@@ -1,9 +1,13 @@
 import { Sidebar } from 'flowbite-react'
 import React,{ useEffect,useState } from 'react'
 import {HiUser,HiArrowSmRight} from 'react-icons/hi'
+import { FaUsers } from "react-icons/fa6";
+import { AiFillPieChart } from "react-icons/ai";
+import { BiSolidCommentDetail } from "react-icons/bi";
+import { BsFileEarmarkPost } from "react-icons/bs";
 import { Link, useLocation } from 'react-router-dom'
 import { signoutSuccess } from '../redux/user/userSlice'
-import { useDispatch } from 'react-redux'
+import { useDispatch, useSelector } from 'react-redux'
 const DashSidebar = () => {
     const location = useLocation()
   const [tab,setTab] = useState('')
@@ -15,6 +19,7 @@ const DashSidebar = () => {
    }
   },[location.search]);
   const dispatch = useDispatch();
+  const {currentUser}= useSelector(state=>state.user);
   const handleSignout =async ()=>{
     try {
       const res= await fetch('/api/user/signout',{
@@ -35,18 +40,47 @@ dispatch(signoutSuccess());
   return (
     
      <Sidebar className='w-full md:w-56'>
-        <Sidebar.ItemGroup>
+        <Sidebar.ItemGroup className='flex flex-col gap-1'>
+        <Sidebar.Item icon={AiFillPieChart} className='cursor-pointer' >
+                  Dashboard
+                </Sidebar.Item>
+
             <Link to='/dashboard?tab=profile'>
                 <Sidebar.Item
                  active={tab==='profile'}
                  icon={HiUser}
-              label={'User'}
+              label={currentUser.isAdmin ? 'Admin':'User'}
                  labelColor='dark'
                  as='div'
                  >
                     Profile
                 </Sidebar.Item>
                 </Link>
+
+                {currentUser.isAdmin && (
+                  <Link to='/dashboard?tab=posts'>
+                <Sidebar.Item 
+                 active={tab==='posts'}
+                icon={BsFileEarmarkPost}
+                labelColor='dark'
+                 as='div'
+                >
+                  Posts
+                </Sidebar.Item>
+                </Link>
+
+                )}
+                
+                
+
+                <Sidebar.Item icon={FaUsers} className='cursor-pointer' >
+                  Users
+                </Sidebar.Item>
+
+                <Sidebar.Item icon={BiSolidCommentDetail} className='cursor-pointer' >
+                  Comments
+                </Sidebar.Item>
+
                 <Sidebar.Item  icon={HiArrowSmRight} className='cursor-pointer'onClick={handleSignout} >
                     Sign Out
                 </Sidebar.Item>
