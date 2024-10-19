0bae775cce428d0c37a62dd8438d4c4292429cb6
aman <amanraj1172004@gmail.com>
2024-10-19 15:28:27 +0530

    showmore

--- frontend/src/components/DashPosts.jsx
+++ frontend/src/components/DashPosts.jsx
@@ -12,6 +12,7 @@ import {
 const DashPosts = () => {
   const { currentUser } = useSelector((state) => state.user);
   const [userPosts, setUserPosts] = useState([]);
+  const [showMore,setShowMore] = useState(true)
 
   useEffect(() => {
     const fetchPosts = async () => {
@@ -32,6 +33,22 @@ const DashPosts = () => {
       fetchPosts();
     }
   }, [currentUser._id]);
+
+  const handleShowMore = async()=>{
+    const startIndex = userPosts.length;
+    try {
+      const res = await fetch(`/api/post/getposts?userId=${currentUser._id}&startIndex=${startIndex}`);
+      const data = await res.json();
+      if(res.ok){
+        setUserPosts((prev)=>[...prev,...data.posts]);
+        if(data.post.length<9){
+          setShowMore(false);
+        }
+      }
+    } catch (error) {
+      console.log(error.message);
+    }
+  }
 
   return (
     <div className="table-auto overflow-x-scroll md:mx-auto p-3 scrollbar scrollbar-track-slate-100 scrollbar-thumb-slate-300 dark:scrollbar-track-slate-700 dark:scrollbar-thumb-slate-500">
@@ -79,6 +96,11 @@ const DashPosts = () => {
               </TableBody>
             ))}
           </Table>
+          {
+            showMore && (
+              <button onClick={handleShowMore} className="w-full text-teal-500 self-center text-sm py-7 ">Show more</button>
+            )
+          }
         </>
       ) : (
         <p>You have no posts yet</p>
