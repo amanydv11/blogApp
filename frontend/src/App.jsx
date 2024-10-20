93f2e70da6a9cc07190c0dece7da43816f1c8e62
aman <amanraj1172004@gmail.com>
2024-10-20 15:43:02 +0530

    update

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -12,6 +12,7 @@ import Footer from './components/Footers'
 import PrivateRoute from './components/PrivateRoute'
 import OnlyAdminPrivateRoute from './components/OnlyAdminPrivateRoute'
 import CreatePost from './pages/CreatePost'
+import UpdatePost from './pages/UpdatePost'
 const App = () => {
   return (
     <>
@@ -29,6 +30,7 @@ const App = () => {
         </Route>
         <Route element={<OnlyAdminPrivateRoute/>}>
         <Route path='/create-post' element={<CreatePost/>} />
+        <Route path='/update-post/:postId' element={<UpdatePost/>} />
         </Route>
       </Routes>
       <Footer/>
