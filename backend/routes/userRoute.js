93c751be65acbf7f68415f33e628d071dbfe6b9d
aman <amanraj1172004@gmail.com>
2024-10-20 17:32:53 +0530

    deluser

--- backend/routes/userRoute.js
+++ backend/routes/userRoute.js
@@ -1,5 +1,5 @@
 import express from 'express'
-import { updateUser,deleteUser, signout } from '../controller/userController.js';
+import { updateUser,deleteUser, signout,getUsers, getUser } from '../controller/userController.js';
 import { verifyToken } from '../utils/verifyUser.js';
 const router = express.Router();
 
@@ -9,5 +9,7 @@ router.get('/test',(req,res)=>{
 router.put('/update/:userId',verifyToken,updateUser);
 router.delete('/delete/:userId',verifyToken,deleteUser)
 router.post('/signout',signout)
+router.get('/getusers', verifyToken, getUsers);
+router.get('/:userId', getUser);
 
 export default router;
--- frontend/src/components/DashSidebar.jsx
+++ frontend/src/components/DashSidebar.jsx
@@ -1,92 +1,97 @@
-import { Sidebar } from 'flowbite-react'
-import React,{ useEffect,useState } from 'react'
-import {HiUser,HiArrowSmRight} from 'react-icons/hi'
+import { Sidebar } from "flowbite-react";
+import React, { useEffect, useState } from "react";
+import { HiUser, HiArrowSmRight } from "react-icons/hi";
 import { FaUsers } from "react-icons/fa6";
 import { AiFillPieChart } from "react-icons/ai";
 import { BiSolidCommentDetail } from "react-icons/bi";
 import { BsFileEarmarkPost } from "react-icons/bs";
-import { Link, useLocation } from 'react-router-dom'
-import { signoutSuccess } from '../redux/user/userSlice'
-import { useDispatch, useSelector } from 'react-redux'
+import { Link, useLocation } from "react-router-dom";
+import { signoutSuccess } from "../redux/user/userSlice";
+import { useDispatch, useSelector } from "react-redux";
 const DashSidebar = () => {
-    const location = useLocation()
-  const [tab,setTab] = useState('')
-  useEffect(()=>{
-    const urlParams = new URLSearchParams(location.search)
-    const tabFromUrl = urlParams.get('tab')
-   if(tabFromUrl){
-    setTab(tabFromUrl)
-   }
-  },[location.search]);
+  const location = useLocation();
+  const [tab, setTab] = useState("");
+  useEffect(() => {
+    const urlParams = new URLSearchParams(location.search);
+    const tabFromUrl = urlParams.get("tab");
+    if (tabFromUrl) {
+      setTab(tabFromUrl);
+    }
+  }, [location.search]);
   const dispatch = useDispatch();
-  const {currentUser}= useSelector(state=>state.user);
-  const handleSignout =async ()=>{
+  const { currentUser } = useSelector((state) => state.user);
+  const handleSignout = async () => {
     try {
-      const res= await fetch('/api/user/signout',{
-        method:'POST',
+      const res = await fetch("/api/user/signout", {
+        method: "POST",
       });
       const data = await res.json();
-      if(!res.ok){
-        console.log(data.message)
+      if (!res.ok) {
+        console.log(data.message);
+      } else {
+        dispatch(signoutSuccess());
       }
-      else{
-dispatch(signoutSuccess());
-      }
-    } catch (error) {
-      
-    }
-    
-  }
+    } catch (error) {}
+  };
   return (
-    
-     <Sidebar className='w-full md:w-56'>
-        <Sidebar.ItemGroup className='flex flex-col gap-1'>
-        <Sidebar.Item icon={AiFillPieChart} className='cursor-pointer' >
-                  Dashboard
-                </Sidebar.Item>
+    <Sidebar className="w-full md:w-56">
+      <Sidebar.ItemGroup className="flex flex-col gap-1">
+        <Sidebar.Item icon={AiFillPieChart} className="cursor-pointer">
+          Dashboard
+        </Sidebar.Item>
 
-            <Link to='/dashboard?tab=profile'>
-                <Sidebar.Item
-                 active={tab==='profile'}
-                 icon={HiUser}
-              label={currentUser.isAdmin ? 'Admin':'User'}
-                 labelColor='dark'
-                 as='div'
-                 >
-                    Profile
-                </Sidebar.Item>
-                </Link>
+        <Link to="/dashboard?tab=profile">
+          <Sidebar.Item
+            active={tab === "profile"}
+            icon={HiUser}
+            label={currentUser.isAdmin ? "Admin" : "User"}
+            labelColor="dark"
+            as="div"
+          >
+            Profile
+          </Sidebar.Item>
+        </Link>
 
-                {currentUser.isAdmin && (
-                  <Link to='/dashboard?tab=posts'>
-                <Sidebar.Item 
-                 active={tab==='posts'}
-                icon={BsFileEarmarkPost}
-                labelColor='dark'
-                 as='div'
-                >
-                  Posts
-                </Sidebar.Item>
-                </Link>
+        {currentUser.isAdmin && (
+          <Link to="/dashboard?tab=posts">
+            <Sidebar.Item
+              active={tab === "posts"}
+              icon={BsFileEarmarkPost}
+              labelColor="dark"
+              as="div"
+            >
+              Posts
+            </Sidebar.Item>
+          </Link>
+        )}
 
-                )}
-                
-                
+        {currentUser.isAdmin && (
+          <Link to="/dashboard?tab=users">
+            <Sidebar.Item
+              icon={FaUsers}
+              active={tab === "users"}
+              labelColor="dark"
+              as="div"
+            >
+              Users
+            </Sidebar.Item>
+          </Link>
+        )}
 
-                <Sidebar.Item icon={FaUsers} className='cursor-pointer' >
-                  Users
-                </Sidebar.Item>
+        <Sidebar.Item icon={BiSolidCommentDetail} className="cursor-pointer">
+          Comments
+        </Sidebar.Item>
 
-                <Sidebar.Item icon={BiSolidCommentDetail} className='cursor-pointer' >
-                  Comments
-                </Sidebar.Item>
+        <Sidebar.Item
+          icon={HiArrowSmRight}
+          className="cursor-pointer"
+          onClick={handleSignout}
+        >
+          Sign Out
+        </Sidebar.Item>
+      </Sidebar.ItemGroup>
+    </Sidebar>
+  );
+};
 
-                <Sidebar.Item  icon={HiArrowSmRight} className='cursor-pointer'onClick={handleSignout} >
-                    Sign Out
-                </Sidebar.Item>
-        </Sidebar.ItemGroup>
-        </Sidebar> 
-  )
-}
-
-export default DashSidebar
+export default DashSidebar;
