da27628b0f54630e53a2c3b6a3aa94a17db0cbb0
aman <amanraj1172004@gmail.com>
2024-11-18 23:41:54 +0530

    homepage

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -1,9 +1,50 @@
-import React from 'react'
-
+import React, { useEffect, useState } from 'react'
+import {Link} from 'react-router-dom'
+import Action from '../components/Action'
+import PostCard from '../components/PostCard'
 const Home = () => {
+  const[posts,setPosts]= useState([]);
+  useEffect(()=>{
+    const fetchPosts = async()=>{
+      const res =await fetch('/api/post/getPosts')
+      const data= await res.json()
+      setPosts(data.posts)
+    }
+    fetchPosts()
+  },[]);
   return (
     <div>
-      <h1>hello baby</h1>
+      <div className=" flex flex-col gap-6 p-28 px-3 max-w-6xl mx-auto ">
+      <h1 className='text-3xl font-serif font-bold lg:text-5xl'>Welcome to my Blog</h1> 
+      <p className='text-gray-500 text-xs sm:text-sm'>Here you'll find a variety of articles and tutorials on topics such as web development,
+        software engineering, and programming languages.
+      </p>
+      <Link to='/search' className='text-xs sm:text-sm text-teal-500 font-bold hover:underline' > View all posts
+</Link>
+      </div>
+<div className="p-3 bg-amber-100 dark:bg-slate-700">
+  <Action/>
+</div>
+<div className="max-w-6xl mx-auto flex flex-col gap-8 py-7">
+     {posts && posts.length > 0 && (
+      <div className='flex flex-col gap-6'>
+        <h2 className='text-2xl font-semibold text-center'>Recent Posts</h2>
+        <div className='justify-center flex flex-wrap gap-6'>
+          {posts.map((post) => (
+            <PostCard key={post._id} post={post} />
+          ))}
+        </div>
+        <Link
+          to={'/search'}
+          className='text-lg text-teal-500 hover:underline text-center'
+        >
+          View all posts
+        </Link>
+        </div>
+    )
+  }
+ 
+</div>
     </div>
   )
 }
