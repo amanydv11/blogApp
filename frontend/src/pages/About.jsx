c70c37f4445109ac5ec01c22e336c6e96dcbc57a
aman <amanraj1172004@gmail.com>
2024-11-30 18:13:31 +0530

    termsconditon complete

--- frontend/src/pages/About.jsx
+++ frontend/src/pages/About.jsx
@@ -1,27 +1,45 @@
-import React from 'react'
+import React from "react";
 
 const About = () => {
   return (
-    <div className='min-h-screen flex items-center justify-center'>
+    <div className="min-h-screen flex items-center justify-center">
       <div className="max-w-2xl mx-auto p-3 text-center">
         <div className="">
-          <h1 className='text-3xl font font-semibold text-center my-7'>About Aman's Blog</h1>
+          <h1 className="text-3xl font-serif font-semibold text-center my-7">
+            About EduGlint
+          </h1>
           <div className="text-md text-gray-500 flex flex-col gap-6">
             <p>
-heyy whatspp
+              Welcome to Eduglint, your comprehensive destination for knowledge
+              and exploration. We are dedicated to providing valuable and
+              engaging content across a wide array of topics, including
+              programming languages, regional insights, culinary delights,
+              educational institutions, and travel experiences.
             </p>
             <p>
-mai hu champion
+              At Eduglint, we aim to empower learners, travelers, and
+              enthusiasts by offering well-researched and reliable information.
+              Our platform serves as a bridge to knowledge, connecting you with
+              the resources you need to expand your horizons and make informed
+              decisions.
             </p>
             <p>
-yaad teri aayegi
+              Whether you are seeking to enhance your technical skills, explore
+              the rich cultural heritage of different states, discover exquisite
+              cuisines, find the right college, or plan your next journey,
+              Eduglint is here to guide you.
+            </p>
+            <p>
+              With a commitment to quality and accuracy, Eduglint is designed to
+              inspire curiosity, spark creativity, and promote lifelong
+              learning. Together, let’s explore the endless possibilities that
+              knowledge can bring.
             </p>
           </div>
         </div>
       </div>
-      
     </div>
-  )
-}
+  );
+};
 
-export default About
+export default About;
--- frontend/src/pages/TermsCondition.jsx
+++ frontend/src/pages/TermsCondition.jsx
@@ -1,17 +1,99 @@
-import React from 'react'
-import Spline from '@splinetool/react-spline';
+import React from "react";
+import Spline from "@splinetool/react-spline";
 const TermsCondition = () => {
   return (
     <div>
-      <Spline
-        scene="https://prod.spline.design/hi7l0exaD4vqlrNn/scene.splinecode" 
-      />
+      <div className="min-h-screen flex items-center justify-center">
+        <div className="m-2 ">
+          <p className="text-2xl font-mono m-5 text-green-500"> Welcome to Eduglint! <br/> By accessing or using our website, you agree to
+          comply with and be bound by the following terms and conditions. Please
+          read them carefully.</p>
+         <div className="p-5">
+          <h1  className='text-3xl text-bold font-serif '> 1. Acceptance of Terms</h1>
+          <p  className='text-gray-500 text-sm'> By using Eduglint, you
+          confirm that you accept these terms and conditions and agree to abide
+          by them. If you do not agree to these terms, you should refrain from
+          using our website.</p>
+         </div>
+          <div className="p-5">
+            <h1 className='text-3xl text-bold  font-serif'>2. User Authentication and Data Collection</h1>
+            <p  className='text-gray-500 text-sm'> To
+          access certain features or services, you may be required to provide
+          your email address. This information is collected solely for the
+          purpose of user authentication and account management. We prioritize
+          the security of your personal data and ensure that it is never shared,
+          sold, or disclosed to third parties, except as required by law.</p>
+          </div>
+          <div className="p-5">
+            <h1 className='text-3xl text-bold font-serif '>3.
+            Cookies and Personalized Content</h1>
+            <p className='text-gray-500 text-sm'>Eduglint uses cookies to enhance your
+          browsing experience. Cookies allow us to understand your preferences
+          and display content that aligns with your interests. By using our
+          website, you consent to the use of cookies. You can adjust your
+          browser settings to manage or disable cookies, though some features of
+          the website may not function properly as a result.</p>
+          </div>
+          <div className="p-5">
+            <h1 className='text-3xl text-bold  font-serif'>4. Use of
+            Information </h1>
+            <p className='text-gray-500 text-sm'> The information provided on Eduglint is for informational
+          and educational purposes only. While we strive for accuracy and
+          reliability, we do not guarantee the completeness or timeliness of the
+          information. Users are encouraged to verify the information
+          independently before making decisions based on it.</p>
+          </div>
+          <div className="p-5">
+            <h1 className='text-3xl text-bold  font-serif'> 5. User
+            Responsibilities</h1>
+            <p className='text-gray-500 text-sm'> When using Eduglint, you agree to: Provide accurate
+          and up-to-date information during registration. Refrain from engaging
+          in activities that may harm, disrupt, or misuse our website or its
+          content. Respect all applicable laws and regulations while using our
+          services.</p>
+          </div>
+          <div className="p-5">
+            <h1 className='text-3xl text-bold  font-serif'>6. Intellectual Property</h1>
+            <p className='text-gray-500 text-sm'> All content on Eduglint, including
+          text, graphics, logos, and design elements, is the property of
+          Eduglint or its content providers. Unauthorized use, reproduction, or
+          distribution of this content is strictly prohibited.</p>
+          </div>
+          <div className="p-5">
+            <h1 className='text-3xl text-bold  font-serif'>7. Third-Party
+            Links </h1>
+            <p className='text-gray-500 text-sm'> Eduglint may contain links to external websites or resources.
+          These links are provided for convenience, and we are not responsible
+          for the content, policies, or practices of third-party websites.</p>
+          </div>
+          <div className="p-5">
+            <h1 className='text-3xl text-bold  font-serif'> 8.
+            Limitation of Liability</h1>
+            <p className='text-gray-500 text-sm'>Eduglint is not liable for any direct,
+          indirect, incidental, or consequential damages resulting from the use
+          or inability to use the website or its content.</p>
+          </div>
+          <div className="p-5">
+            <h1 className='text-3xl text-bold font-serif '>9. Modifications to
+            Terms</h1>
+            <p className='text-gray-500 text-sm'>Eduglint reserves the right to update or modify these terms and
+          conditions at any time without prior notice. Continued use of the
+          website after changes have been made constitutes acceptance of the
+          updated terms.</p>
+          </div>
+          <div className="p-5">
+            <h1 className='text-3xl text-bold font-serif'> 10. Contact Information</h1>
+            <p className='text-gray-500 text-sm'>If you have any questions or
+          concerns about these terms and conditions, please contact us at
+          [eduglint@proton.me]. By using Eduglint, you acknowledge that
+          you have read, understood, and agreed to these terms and conditions.</p>
+          </div>
+
+                  
+        </div>
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default TermsCondition
-
-
-
-
+export default TermsCondition;
